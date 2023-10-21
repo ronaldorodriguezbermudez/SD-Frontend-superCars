7fd9673aa827041d965c3665cedd64e218eb24ee
Ronaldo Rodríguez Bermúdez <r2.bermudez@gmail.com>
2023-10-21 16:43:35 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,14 @@
         to="/manufacturer">Manufacturers</router-link>
     </div>
    <router-view />
+   <div class="row"  style="margin-top: 15px">
+      <router-link class="three columns button button-primary" 
+        to="/https://proyecto2-sd-backend.netlify.app/.netlify/functions/modelTasks">Run Queues Models</router-link>
+      <router-link class="three columns button button-primary" 
+        to="/designer">Designers</router-link>
+      <router-link class="three columns button button-primary" 
+        to="/manufacturer">Manufacturers</router-link>
+    </div>
     <div class="row">
       <button disabled="disabled" class="twelve columns button-primary">
         Copyright (c) 2023 - Ronaldo Rodriguez | Brandon Rodriguez
