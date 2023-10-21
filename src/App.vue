2a08ecde1165bf18df697711012035fa50ac5bb1
Ronaldo Rodríguez Bermúdez <r2.bermudez@gmail.com>
2023-10-21 16:50:15 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app" class="container">
-    <div class="row"  style="margin-top: 15px">
-      <router-link class="three columns button button-primary" 
-        to="/">Home</router-link>
-      <router-link class="three columns button button-primary" 
-        to="/model">Models</router-link>
-      <router-link class="three columns button button-primary" 
-        to="/designer">Designers</router-link>
-      <router-link class="three columns button button-primary" 
-        to="/manufacturer">Manufacturers</router-link>
+    <div class="row" style="margin-top: 15px">
+      <router-link class="three columns button button-primary" to="/">Home</router-link>
+      <router-link class="three columns button button-primary" to="/model">Models</router-link>
+      <router-link class="three columns button button-primary" to="/designer">Designers</router-link>
+      <router-link class="three columns button button-primary" to="/manufacturer">Manufacturers</router-link>
     </div>
-   <router-view />
-   <div class="row"  style="margin-top: 15px">
-      <router-link class="three columns button button-primary" 
-        to="https://proyecto2-sd-backend.netlify.app/.netlify/functions/modelTasks">Run Queues Models</router-link>
-      <router-link class="three columns button button-primary" 
-        to="/designer">Designers</router-link>
-      <router-link class="three columns button button-primary" 
-        to="/manufacturer">Manufacturers</router-link>
+    <router-view />
+    <div class="row" style="margin-top: 15px">
+      <button class="three columns button button-primary" @click="runQueue('modelTasks')">Run Queues Models</button>
+      <button class="three columns button button-primary" @click="runQueue('designerTasks')">Run Queues Designers</button>
+      <button class="three columns button button-primary" @click="runQueue('manufacturerTasks')">Run Queues Manufacturers</button>
     </div>
     <div class="row">
       <button disabled="disabled" class="twelve columns button-primary">
@@ -25,10 +18,31 @@
       </button>
     </div>
   </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "App",
-  };
-  </script>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    runQueue(queueName) {
+      // Perform a fetch request to the specified URL
+      fetch(`https://proyecto2-sd-backend.netlify.app/.netlify/functions/${queueName}`)
+        .then((response) => {
+          if (response.ok) {
+            return response.text();
+          } else {
+            throw new Error("Request failed");
+          }
+        })
+        .then((data) => {
+          // Handle the response data as needed
+          console.log(data);
+        })
+        .catch((error) => {
+          // Handle errors, e.g., show an error message to the user
+          console.error("Request error:", error);
+        });
+    },
+  },
+};
+</script>
